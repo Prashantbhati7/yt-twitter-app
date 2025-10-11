b526e844c110129bb59155c1ff55ef2d97c089c9
Prashant Bhati <prashantbhati774@gmail.com>
2025-10-11 22:32:21 +0530

    add: extend user routes to include account management and watch history features

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import {LoginUser, logoutUser, registeruser,refreshAccessToken} from "../controllers/user.controller.js";
+import {LoginUser, logoutUser, registeruser,refreshAccessToken, changePassword, getCurrUser, UpdateAccountDetails, updataAvatar, updateCover, getUserChannelProfile, getWatchHistory} from "../controllers/user.controller.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 import { upload } from "../middlewares/multer.middleware.js";
 const router = Router();
@@ -15,5 +15,11 @@ router.route("/login").post(LoginUser);
 router.route("/logout").post(verifyJWT,logoutUser);
 
 router.route("/refresh-route").post(refreshAccessToken);
-
+router.route("/cahnge-password").post(verifyJWT,changePassword)
+router.route("/curruser").get(verifyJWT,getCurrUser)
+router.route("/update-account").patch(verifyJWT,UpdateAccountDetails)
+router.route("/avatar").patch(verifyJWT,upload.single("avatar"),updataAvatar);
+router.route("/cover-image").patch(verifyJWT,upload.single("coverImage"),updateCover);
+router.route("/c/:username").get(verifyJWT,getUserChannelProfile);
+router.route("/watch-history").get(verifyJWT,getWatchHistory);
 export default router;
